a97a34ebc16a6b15a9df3d54d0fb81f800dc1c1d
PLDaily <pcd12321@gmail.com>
2018-01-10 19:54:36 +0800

    add coverage

---
+++ karma.conf.js
@@ -0,0 +1,46 @@
+// Karma configuration
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'test/*.js'
+    ],
+    exclude: [],
+    preprocessors: {
+      'test/*.js': ['webpack'],
+      'index.js': ['coverage']
+    },
+    reporters: ['progress', 'coverage', 'coveralls'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    concurrency: Infinity,
+    webpack: {
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+      }
+    },
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
+    plugins: [
+      'karma-webpack',
+      'karma-phantomjs-launcher',
+      'karma-sourcemap-loader',
+      'karma-coverage',
+      'karma-mocha',
+      'karma-chai'
+    ],
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,15 +1,26 @@
-mocha.ui('bdd')
-
-var expect = chai.expect
-describe("Tests", function () {
-  before(function () {
+const showToast = require('../index.js');
+describe('showToast Test1', function () {
+  before(function() {
     showToast('this is a toast')
   })
-  it("content right", function () {
+  it("right", function () {
     var el = document.querySelector('.show-toast')
     expect(el).to.not.equal(null)
     expect(el.innerHTML).to.equal("this is a toast")
   })
 })
 
-mocha.run()
+describe('showToast Test2', function () {
+  before(function() {
+    showToast({
+      str: 'this is a toast1',
+      time: 3000,
+      position: 'bottom'
+    })
+  })
+  it("right", function () {
+    var el = document.querySelector('.show-toast')
+    expect(el).to.not.equal(null)
+    expect(el.innerHTML).to.equal("this is a toast1")
+  })
+})
